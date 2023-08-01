e9fe5eb08800ab8539832ab8c359946bf3975064
Max Slidzen <slidzon94@gmail.com>
2023-08-02 01:31:56 +0300

    changed admin and fixtures

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from catalog.models import Product, Category
 
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'category', 'price', 'created_at', 'changed_at')
-    list_filter = ('name',)
+    list_display = ('id', 'name', 'category', 'price')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
+    list_display = ('id', 'name')
