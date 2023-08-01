3d7c005fb9039aa8dcbe287e76e1dd52f5024911
Max Slidzen <slidzon94@gmail.com>
2023-08-02 02:45:23 +0300

    Created custom command

---
+++ catalog/management/commands/fill_db.py
@@ -0,0 +1,26 @@
+﻿from django.core.management import BaseCommand, call_command
+
+from catalog.models import Product, Category
+import json
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **kwargs):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+        # product_list = []
+        # category_list = []
+        # with open('catalog.json', 'r', encoding='utf-8') as f:
+        #     data = json.load(f)
+        #     for item in data:
+        #         if item['model'] == 'catalog.category':
+        #             category_list.append(Category(pk=item['pk'], **item['fields']))
+        #
+        #         С продуктами не работает из-за FK
+        #         elif item['model'] == 'catalog.product':
+        #             product_list.append(Product(pk=item['pk'], **item['fields']))
+        #
+        # Category.objects.bulk_create(category_list)
+        # Product.objects.bulk_create(product_list)
+        call_command('loaddata', 'catalog.json')
