f1d06e05b7eed023d03e95be604579545c253cac
max_slidzen <slidzon94@gmail.com>
2023-08-14 13:52:48 +0300

    refactored home and contacts

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Product
+from random import shuffle
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+
+    context = {
+        'title': 'Главная',
+    }
+    return render(request, 'catalog/home.html', context)
 
 
 def contacts(request):
@@ -13,5 +20,28 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
 
+    context = {
+        'title': 'Контакты'
+    }
+    return render(request, 'catalog/contacts.html', context)
+
+
+# def catalog(request):
+#
+#     context = {
+#         'object_list': Product.objects.all(),
+#         'title': 'Каталог',
+#     }
+#     return render(request, 'catalog/catalog.html', context)
+
+
+# def home(request):
+#     product = Product.objects.all()
+#     product_list.shuffle()
+#
+#     context = {
+#         'object_list': product_list[:5],
+#         'title': 'Главная',
+#     }
+#     return render(request, 'catalog/home.html', context)
