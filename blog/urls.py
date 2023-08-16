335abf6b2e5679a7840335da194d4f23af7c827a
max_slidzen <slidzon94@gmail.com>
2023-08-17 00:31:04 +0300

    added detail view for articles

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import ArticleListView
+from blog.views import ArticleListView, ArticleDetailView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article_list'),
+    path('view/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     # path('contacts/', MessageCreateView.as_view(), name='contacts'),
-    # path('view/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from blog.models import Article
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 class ArticleListView(ListView):
@@ -8,4 +8,11 @@ class ArticleListView(ListView):
     extra_context = {
             'title': 'Блог',
             'button': 'Перейти к статье'
+        }
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    extra_context = {
+            'title': 'Статья',
         }
