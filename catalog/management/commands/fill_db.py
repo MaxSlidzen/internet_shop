9a51deb5c6dba839f95a1ae74f4e1216c3fa9dec
max_slidzen <slidzon94@gmail.com>
2023-08-17 03:24:14 +0300

    updated custom command

--- catalog/management/commands/fill_db.py
+++ catalog/management/commands/fill_db.py
@@ -1,7 +1,7 @@
 ﻿from django.core.management import BaseCommand, call_command
 
-from catalog.models import Product, Category
-import json
+from catalog.models import Product, Category, Message
+from blog.models import Article
 
 
 class Command(BaseCommand):
@@ -9,18 +9,8 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         Product.objects.all().delete()
         Category.objects.all().delete()
-        # product_list = []
-        # category_list = []
-        # with open('catalog.json', 'r', encoding='utf-8') as f:
-        #     data = json.load(f)
-        #     for item in data:
-        #         if item['model'] == 'catalog.category':
-        #             category_list.append(Category(pk=item['pk'], **item['fields']))
-        #
-        #         С продуктами не работает из-за FK
-        #         elif item['model'] == 'catalog.product':
-        #             product_list.append(Product(pk=item['pk'], **item['fields']))
-        #
-        # Category.objects.bulk_create(category_list)
-        # Product.objects.bulk_create(product_list)
-        call_command('loaddata', 'catalog.json')
+        Message.objects.all().delete()
+        Article.objects.all().delete()
+
+        call_command('loaddata', 'catalog_data.json')
+        call_command('loaddata', 'blog_data.json')
