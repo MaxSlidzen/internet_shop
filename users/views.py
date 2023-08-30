00ede6131dc6f53ef1492ed71d612c04f3c22d38
max_slidzen <slidzon94@gmail.com>
2023-08-30 14:26:33 +0300

    added registration and change profile

--- users/views.py
+++ users/views.py
@@ -1,2 +1,27 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView
 
+from users.forms import UserRegisterForm, UserProfileForm, UserLoginForm
+from users.models import User
+
+
+class UserLoginView(LoginView):
+    model = User
+    form_class = UserLoginForm
+
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+
+class UserProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('catalog:product_list')
+
+    def get_object(self, queryset=None):
+        return self.request.user
